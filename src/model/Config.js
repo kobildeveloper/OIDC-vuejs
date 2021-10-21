2ed24a88e678ceeb647b3f819d10d25fb83b81be
Sriram Karthick <sriram.karthick@grootan.com>
2021-10-21 13:15:06 +0530

    Updated Config
    Updated Config

--- src/model/Config.js
+++ src/model/Config.js
@@ -1,6 +1,6 @@
 export const CookieName = "midentity_Auth";
 export const Config = {
-    "url": "https://banking.api.midentitybox.com/auth",
-    "realm": "banking",
-    "clientId": "kong"
+    "url": "https://{partnerId}.{hostname}/auth",
+    "realm": "your-midentity-box-oidc-tenant-id",
+    "clientId": "your-midentity-box-oidc-app-client-id"
 }
